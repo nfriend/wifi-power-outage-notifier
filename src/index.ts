9c264c46f75175b28e9aa261e7dea6dba4940dcf
Nathan Friend <nathan@gitlab.com>
2019-11-23 17:08:08 -0400

    Testing Lambda deploy

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('hello!');
+module.exports.checkForRecentPings = async () => {
+  console.log('checkForRecentPings is executing');
+
+  const iftttKeys = JSON.parse(process.env.IFTTT_KEYS);
+  const awsDynamoRegion = JSON.parse(process.env.AWS_DYNAMO_REGION);
+
+  console.log('iftttKeys:', iftttKeys);
+  console.log('awsDynamoRegion:', awsDynamoRegion);
+
+  return {
+    statusCode: 200,
+    body: 'checkForRecentPings executed successfully!',
+  };
+};
+
+module.exports.ping = async () => {
+  console.log('ping is executing');
+
+  const awsDynamoRegion = JSON.parse(process.env.AWS_DYNAMO_REGION);
+
+  console.log('awsDynamoRegion:', awsDynamoRegion);
+
+  return {
+    statusCode: 200,
+    body: 'ping executed successfully!',
+  };
+};
